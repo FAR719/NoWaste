74a4223a285f97c80b2c86d1203a2a1876baa274
francesco_fiorella <francescofio99@gmail.com>
2020-11-17 22:48:48 +0100

    toolbar added to CardDetailActivity.java

--- app/src/main/java/com/far/nowaste/CardDetailActivity.java
+++ app/src/main/java/com/far/nowaste/CardDetailActivity.java
@@ -1,17 +1,26 @@
 package com.far.nowaste;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.widget.TextView;
 
 public class CardDetailActivity extends AppCompatActivity {
+
+    // toolbar
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_card_detail);
+
+        // toolbar
+        mToolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(mToolbar);
 
         // definizione variabili
         TextView detailsTextView = (TextView) findViewById(R.id.detailsTextView);
@@ -22,5 +31,13 @@ public class CardDetailActivity extends AppCompatActivity {
         // display the name
         String wasteType = in.getStringExtra("com.far.nowaste.CARD_TYPE");
         detailsTextView.setText(wasteType);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
     }
 }
