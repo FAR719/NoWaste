a60889263d3ed3adb43988ce645fce147dec8e3a
francesco_fiorella <francescofio99@gmail.com>
2020-11-17 21:27:30 +0100

    navDrawer implementato nelle main (sto per eliminare alcuni file)

--- app/src/main/java/com/far/nowaste/MainActivity.java
+++ app/src/main/java/com/far/nowaste/MainActivity.java
@@ -1,20 +1,30 @@
 package com.far.nowaste;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.cardview.widget.CardView;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.SearchView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     // toolbar
     private Toolbar mToolbar;
+
+    // navigationView
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     //definizione variabili
     SearchView wasteSearchView;
@@ -33,8 +43,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // toolbar
-        mToolbar = findViewById(R.id.main_toolbar);
+        mToolbar = (Toolbar) findViewById(R.id.main_toolbar);
         setSupportActionBar(mToolbar);
+
+        // navigationView
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView)findViewById(R.id.nav_view);
 
         // collega le view
         wasteSearchView = (SearchView)findViewById(R.id.wasteSearchView);
@@ -46,6 +60,12 @@ public class MainActivity extends AppCompatActivity {
         metalliCardView = (CardView)findViewById(R.id.metalliCardView);
         elettriciCardView = (CardView)findViewById(R.id.elettriciCardView);
         specialiCardView = (CardView)findViewById(R.id.specialiCardView);
+
+        // navigationView
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, mToolbar, R.string.openNavDrawer, R.string.closeNavDrawer);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
 
         // definizione onClick searchView
         wasteSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
@@ -142,5 +162,10 @@ public class MainActivity extends AppCompatActivity {
 
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
     }
 }
