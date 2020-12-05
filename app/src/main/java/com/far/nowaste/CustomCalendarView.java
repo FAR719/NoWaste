d9c92d0ea163806ccff5bc6b5c1f3371ae2e0555
renato <nocerinorenato99@gmail.com>
2020-12-05 22:25:59 +0100

    implementazione CustomCalendarView

--- app/src/main/java/com/far/nowaste/CustomCalendarView.java
+++ app/src/main/java/com/far/nowaste/CustomCalendarView.java
@@ -1,4 +1,37 @@
 package com.far.nowaste;
 
-public class CustomCalendarView {
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.GridView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+public class CustomCalendarView extends LinearLayout {
+
+    // dichiarazione variabili
+    TextView currentDate;
+    GridView gridView;
+    private static final int Max_Calendar_Days = 42;
+    Calendar calendar = Calendar.getInstance(Locale.ITALIAN);
+    Context context;
+
+    // lista data e lista eventi
+    List<Date> dates = new ArrayList<>();
+    List<Events> eventsList = new ArrayList<>();
+
+    public CustomCalendarView(Context context) {
+        super(context);
+    }
+
+    public CustomCalendarView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
 }
