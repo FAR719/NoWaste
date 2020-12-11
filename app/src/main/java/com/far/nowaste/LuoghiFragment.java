edead2e4aa51ecaed516ed5c7e8443c98986d83a
renato <nocerinorenato99@gmail.com>
2020-12-11 11:26:39 +0100

    implementati metodi in LuoghiFragment per i permessi della posizione

--- app/src/main/java/com/far/nowaste/LuoghiFragment.java
+++ app/src/main/java/com/far/nowaste/LuoghiFragment.java
@@ -1,5 +1,9 @@
 package com.far.nowaste;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,11 +12,19 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 public class LuoghiFragment extends AppCompatActivity {
 
@@ -30,7 +42,71 @@ public class LuoghiFragment extends AppCompatActivity {
         // inizializzazione FusedLocation
         client = LocationServices.getFusedLocationProviderClient(this);
 
+        // Check permessi
+        if (ActivityCompat.checkSelfPermission(LuoghiFragment.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            // permesso concesso
+            getCurrentLocation();
+        } else {
+            // permesso negato
+            // RICHIESTA PERMESSO
+            ActivityCompat.requestPermissions(LuoghiFragment.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},44);
+        }
 
     }
 
+    private void getCurrentLocation() {
+        // permessi per usare getLastLocation
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+        // Inizializzazione task Location
+        Task<Location> task = client.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                // Se ha successo
+
+                if(location != null){
+                    // Sincronizza Mappa
+                    supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+                        @Override
+                        public void onMapReady(GoogleMap googleMap) {
+                            // inizializzazione Latitudine e Longitudine
+                            LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());
+
+                            // marker per segnalare la posizione attuale
+                            MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("Sono qui!");
+
+                            // Zoom Mappa
+                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,10));
+
+                            // aggiungere il marker sulla mappa
+                            googleMap.addMarker(markerOptions);
+                        }
+                    });
+                }
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 44){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                //Quando Permesso concesso
+                getCurrentLocation();
+            }
+        }
+    }
 }
