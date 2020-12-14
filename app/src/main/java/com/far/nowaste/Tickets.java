6d42a1e527d070df2b5463a1041cf5c2fd6770ea
Re7A <74328924+Re7A@users.noreply.github.com>
2020-12-14 23:28:33 +0100

    implementata classe Tickets

--- app/src/main/java/com/far/nowaste/Tickets.java
+++ app/src/main/java/com/far/nowaste/Tickets.java
@@ -1,4 +1,34 @@
 package com.far.nowaste;
 
 public class Tickets {
+    // variabili
+    public String oggetto, emailT, data;
+    public boolean stato;
+
+    // costruttore
+    public Tickets(String oggetto, String emailT, String data, boolean stato) {
+        this.oggetto = oggetto;
+        this.emailT = emailT;
+        this.data = data;
+        this.stato = stato;
+    }
+
+    //costruttore vuoto per firebase
+    private Tickets(){}
+
+    public String getOggetto() {
+        return oggetto;
+    }
+
+    public String getEmailT() {
+        return emailT;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public boolean isStato() {
+        return stato;
+    }
 }
