0405300381922c45efb99bc6724bd489c1598994
Re7A <74328924+Re7A@users.noreply.github.com>
2020-12-15 22:31:18 +0100

    toolbar aggiunta in TicketChatActivity

--- app/src/main/java/com/far/nowaste/TicketChatActivity.java
+++ app/src/main/java/com/far/nowaste/TicketChatActivity.java
@@ -1,14 +1,30 @@
 package com.far.nowaste;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class TicketChatActivity extends AppCompatActivity {
+    // definizione variabili
+    Toolbar mToolbar;
+    RecyclerView mFirestoreList;
+    FirebaseFirestore firebaseFirestore;
+    FirestoreRecyclerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ticket_chat);
+
+        // toolbar
+        mToolbar = findViewById(R.id.ticketChat_toolbar);
+        setSupportActionBar(mToolbar);
+        mToolbar.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.primary));
     }
 }
