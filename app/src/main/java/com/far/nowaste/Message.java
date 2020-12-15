c304691967e6ec970f236e2c7da41e470250bcd5
Re7A <74328924+Re7A@users.noreply.github.com>
2020-12-15 22:18:15 +0100

    implementata classe Message

--- app/src/main/java/com/far/nowaste/Message.java
+++ app/src/main/java/com/far/nowaste/Message.java
@@ -1,4 +1,25 @@
 package com.far.nowaste;
 
 public class Message {
+    // variabili
+    String descrizione;
+    boolean operatore;
+
+    public Message(String descrizione, boolean operatore) {
+        this.descrizione = descrizione;
+        this.operatore = operatore;
+    }
+
+    // costruttore vuoto per Firebase
+    private Message(){}
+
+    // getter
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public boolean isOperatore() {
+        return operatore;
+    }
 }
