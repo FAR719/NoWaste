cd53ae63b2ddf89cbaa8bd0b37d0b81f0d880a01
Re7A <74328924+Re7A@users.noreply.github.com>
2020-12-18 22:22:30 +0100

    creata ReportErrorActivity

---
+++ app/src/main/java/com/far/nowaste/Other/ReportErrorActivity.java
@@ -0,0 +1,16 @@
+package com.far.nowaste.Other;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.far.nowaste.R;
+
+public class ReportErrorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report_error);
+    }
+}
