c3432c890dfe2717001d122cd02a848ddf0338d3
Re7A <74328924+Re7A@users.noreply.github.com>
2020-12-21 16:05:58 +0100

    pulsanti collegati

--- app/src/main/java/com/far/nowaste/TabTicketActivity.java
+++ app/src/main/java/com/far/nowaste/TabTicketActivity.java
@@ -1,5 +1,7 @@
 package com.far.nowaste;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -14,6 +16,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.OvershootInterpolator;
 
 import com.far.nowaste.ui.main.SectionsPagerAdapter;
 
@@ -21,6 +24,16 @@ public class TabTicketActivity extends AppCompatActivity {
 
     // definizione variabili
     Toolbar mToolbar;
+
+    // view
+    FloatingActionButton newTicketBtn;
+    FloatingActionButton checkBtn;
+    FloatingActionButton errorBtn;
+
+    // animazioni
+    boolean isMenuOpen;
+    OvershootInterpolator interpolator = new OvershootInterpolator();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,5 +54,73 @@ public class TabTicketActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        // imposta l'animazione del floating button
+        initFloatingMenu();
+
+    }
+
+    private void initFloatingMenu() {
+        // ClickListner del bottone
+        newTicketBtn = findViewById(R.id.newTicket_addFloatingActionButton);
+        checkBtn = findViewById(R.id.newTicket_checkFloatingActionButton);
+        errorBtn = findViewById(R.id.newTicket_errorFloatingActionButton);
+
+        checkBtn.setAlpha(0f);
+        checkBtn.setTranslationY(100f);
+        checkBtn.setScaleX(0.7f);
+        checkBtn.setScaleY(0.7f);
+
+        errorBtn.setAlpha(0f);
+        errorBtn.setTranslationY(100f);
+        errorBtn.setScaleX(0.7f);
+        errorBtn.setScaleY(0.7f);
+
+        Intent intentnewTick = new Intent(this, NewTicketActivity.class);
+        Intent intentError = new Intent(this, ReportProblemActivity.class);
+
+        newTicketBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animeteFloatingMenu();
+            }
+        });
+
+        checkBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intentnewTick);
+                animeteFloatingMenu();
+            }
+        });
+
+        errorBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intentError);
+                animeteFloatingMenu();
+            }
+        });
+    }
+
+    private void animeteFloatingMenu() {
+        if(isMenuOpen){
+            newTicketBtn.animate().setInterpolator(interpolator).rotation(0f).setDuration(300).start();
+            checkBtn.animate().translationY(100f).alpha(0f).setInterpolator(interpolator).setDuration(300).start();
+            errorBtn.animate().translationY(100f).alpha(0f).setInterpolator(interpolator).setDuration(300).start();
+
+            Drawable defaulImage = ContextCompat.getDrawable(getApplicationContext(),R.drawable.ic_create);
+            newTicketBtn.setImageDrawable(defaulImage);
+
+            isMenuOpen = false;
+        }else {
+            newTicketBtn.animate().setInterpolator(interpolator).rotation(90f).setDuration(300).start();
+            checkBtn.animate().translationY(15f).alpha(1f).setInterpolator(interpolator).setDuration(300).start();
+            errorBtn.animate().translationY(55f).alpha(1f).setInterpolator(interpolator).setDuration(300).start();
+
+            Drawable defaulImage = ContextCompat.getDrawable(getApplicationContext(),R.drawable.ic_clear);
+            newTicketBtn.setImageDrawable(defaulImage);
+
+            isMenuOpen = true;
+        }
     }
 }
