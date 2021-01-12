3af345d0ba6fc7da58bfa593206f7ff40aa39322
Re7A <74328924+Re7A@users.noreply.github.com>
2021-01-12 16:40:06 +0100

    creata DetailFunzionalitaActivity

---
+++ app/src/main/java/com/far/nowaste/DetailFunzionalitaActivity.java
@@ -0,0 +1,14 @@
+package com.far.nowaste;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailFunzionalitaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_funzionalita);
+    }
+}
