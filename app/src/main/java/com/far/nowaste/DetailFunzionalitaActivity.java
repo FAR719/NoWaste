75269c010517b211a37431f6678c04b38093b6df
Re7A <74328924+Re7A@users.noreply.github.com>
2021-01-20 19:36:52 +0100

    minor fix( non funz)

--- app/src/main/java/com/far/nowaste/DetailFunzionalitaActivity.java
+++ app/src/main/java/com/far/nowaste/DetailFunzionalitaActivity.java
@@ -3,15 +3,22 @@ package com.far.nowaste;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.far.nowaste.objects.Funzionalita;
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 
@@ -58,7 +65,30 @@ public class DetailFunzionalitaActivity extends AppCompatActivity {
 
         // query
         FirebaseFirestore fstore = FirebaseFirestore.getInstance();
-        Query query = fstore.collection("funzionalita").document(nome).collection("faq")
+        Query query = fstore.collection("funzionalita").document(nome).collection("faq");
+
+        // recyclerOptions
+        FirestoreRecyclerOptions<Funzionalita> options = new FirestoreRecyclerOptions.Builder<Funzionalita>().setQuery(query, Funzionalita.class).build();
+
+        adapter = new FirestoreRecyclerAdapter<Funzionalita, DetailFunzionalitaActivity.FAQViewHolder>(options) {
+            @NonNull
+            @Override
+            public DetailFunzionalitaActivity.FAQViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_recycler_view_faq_item parent, false);
+                return new DetailFunzionalitaActivity().FAQViewHolder(view);
+            }
+
+            @Override
+            protected void onBindViewHolder(@NonNull DetailFunzionalitaActivity.FAQViewHolder holder, int position, @NonNull Funzionalita model) {
+                holder.rDomanda.setText(model.getNome());
+                holder.rRisposta.setText(model.getTesto());
+            }
+        };
+
+        // View Holder
+        mFAQList.setHasFixedSize(true);
+        mFAQList.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        mFAQList.setAdapter(adapter);
     }
 
     // ends this activity (back arrow)
@@ -70,5 +100,20 @@ public class DetailFunzionalitaActivity extends AppCompatActivity {
             this.finish();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private class FAQViewHolder extends RecyclerView.ViewHolder{
+
+        TextView rDomanda, rRisposta;
+        ImageView rArrowBtn;
+        ConstraintLayout itemLayout;
+
+        public FAQViewHolder(@NonNull View itemView) {
+            super(itemView);
+            rDomanda = itemView.findViewById(R.id.recView_faq_domandaTextView);
+            rRisposta = itemView.findViewById(R.id.recView_faq_rispostaTextView);
+            rArrowBtn = itemView.findViewById(R.id.recView_faq_ImageBtn);
+            itemLayout = itemView.findViewById(R.id.recView_assItem_constraintLayout);
+        }
     }
 }
